80d307ea29d99195561200f7853c84cc59fe8a11
Ivonei Balena <balenaultra@users.noreply.github.com>
2021-03-31 22:21:17 -0300

    login api criação do refresh token

--- lib/api/login_api.dart
+++ lib/api/login_api.dart
@@ -5,6 +5,31 @@ import 'package:solicitacoes_app/datas/usuario_data.dart';
 import 'package:solicitacoes_app/utils/http_helper.dart' as http;
 
 class LoginApi {
+  static Future<ApiResponse> refreshToken() async {
+    try {
+      var url = 'http://localhost:3000/users/refresh-token';
+
+      var response = await http.post(url);
+
+      Map mapResponse = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        final user = Usuario.fromJson(mapResponse["user"]);
+        user.token = mapResponse["token"];
+        DateTime now = DateTime.now();
+        user.tokenDate = DateTime(now.year, now.month, now.day);
+
+        user.save();
+
+        return ApiResponse.ok(user);
+      }
+      return ApiResponse.error(mapResponse["message"]);
+    } catch (error) {
+
+      return ApiResponse.error("Não foi possível atualizar o token.");
+    }
+  }
+
   static Future<ApiResponse<Usuario>> login(String companyCPFCNPJ, String code, String password) async {
     try {
       var url = 'http://localhost:3000/users/authenticate';
@@ -17,22 +42,23 @@ class LoginApi {
 
       var response = await http.post(url, body: s);
 
-      print('Response status: ${response.statusCode}');
-      print('Response body: ${response.body}');
+      //print('Response body: ${response.body}');
 
       Map mapResponse = json.decode(response.body);
 
       if (response.statusCode == 200) {
         final user = Usuario.fromJson(mapResponse["user"]);
         user.token = mapResponse["token"];
+        DateTime now = DateTime.now();
+        user.tokenDate = DateTime(now.year, now.month, now.day);
 
         user.save();
 
         return ApiResponse.ok(user);
       }
-      return ApiResponse.error(mapResponse["error"]);
+      return ApiResponse.error(mapResponse["message"]);
     } catch (error) {
-      print("Erro no login $error");
+      //print("Erro no login $error");
 
       return ApiResponse.error("Não foi possível fazer o login.");
     }
